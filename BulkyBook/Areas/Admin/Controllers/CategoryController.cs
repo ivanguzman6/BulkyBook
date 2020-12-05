7b618509d68ebec231078f551e4ac4ff717c2ab0
Ivan Guzman Brito <ivanguzman6@hotmail.com>
2020-12-04 22:01:22 -0400

    Se creo un controlador para category con la ayuda de visutal studios

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CategoryController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;                
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        public IActionResult GetAll()
+        {
+            var _allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = _allObj });
+        }
+
+        #endregion
+    }
+}
