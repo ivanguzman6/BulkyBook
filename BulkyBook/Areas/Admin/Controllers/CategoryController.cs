dbb3ef4765757e1e8bd68dfa172c99d6fd7d595d
Ivan Guzman Brito <ivanguzman6@hotmail.com>
2020-12-09 22:13:40 -0400

    Se termino con el upsert y se agrego el delete. NOTA: Al borrar no esta actualizando la lista

--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -40,12 +40,50 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(category);
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                if(category.Id==0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(category);
+        }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var _allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = _allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = false, message = "Delete successful" });
+
+
         }
 
         #endregion
